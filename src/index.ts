5b756188479d26b0bf9e23cb8c0db9d24f424e39
phoocf81 <phoocf81@gmail.com>
2025-09-19 14:39:02 +0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { getAssetFromKV, serveStatic } from '@cloudflare/kv-asset-handler';
+
+    addEventListener('fetch', event => {
+        event.respondWith(handleEvent(event));
+    });
+
+    async function handleEvent(event: FetchEvent) {
+        try {
+            return await getAssetFromKV(event);
+        } catch (e) {
+            // If the asset is not found, try serving index.html for SPAs
+            if (e.name === 'NotFoundError') {
+                return await getAssetFromKV(event, {
+                    mapRequestToAsset: req => serveStatic(req, {
+                        path: 'index.html' // Serve index.html for all unfound paths
+                    }),
+                });
+            }
+            return new Response('Error serving asset', { status: 500 });
+        }
+    }
